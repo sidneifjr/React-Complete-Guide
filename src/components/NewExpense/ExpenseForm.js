0ea11b68397ee3336dfa1913d7ef82d1b7e9c05d
Sidnei Farias <sidneijr.77@gmail.com>
2022-01-19 21:42:29 -0300

    updating to lesson 57

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,49 +2,77 @@ import React, { useState } from 'react';
 import './expenseForm.css';
 
 const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState(''); // é uma string vazia, pois o valor ainda não existe quando o componente é desenhado pela primeira vez.
-  // const [enteredAmount, setEnteredAmount] = useState('');
-  // const [enteredDate, setEnteredDate] = useState('');
+  // No entanto, o curso irá continuar com ênfase ao gerenciamento de múltiplos states.
+  const [enteredTitle, setEnteredTitle] = useState(''); // é uma string vazia, pois o valor ainda não existe quando o componente é desenhado pela primeira vez.
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
   // Uma forma menos repetitiva de usar state slices.
   // Porém, um ponto negativo dessa forma é que, ao atualizar o valor de uma propriedade, você precisa alterar todas as três.
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: ''
-  });
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: ''
+  // });
 
   const titleChangeHandler = event => {
-    // setEnteredTitle(event.target.value);
+    setEnteredTitle(event.target.value);
 
     // O state antigo será substituído pelo novo, definido abaixo. Portanto, para evitar a remoção das outras propriedades, as mesmas devem ser copiadas novamente.
     // Uma forma é usando o 'spread' operator. Ele pega um objeto, obtém todas os pares chave-valor e os adiciona ao novo objeto.
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.title,
-    });
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.title,
+    // });
+
+    // Obtendo o snapshot do state anterior e retornando a snapshot do novo state.
+    // A forma anterior e essa funcionam bem na maioria dos casos.
+    // Porém, é importante notar que o React agenda atualizações de state, não as realiza instantaneamente.
+    // Portanto, se você agendar muitas atualizações de state ao mesmo tempo, é perfeitamente possível que você esteja usando uma snapshot incorreta ou desatualizada do state, se usar o método anterior.
+    // Dessa forma, o React consegue garantir que a snapshot do state será SEMPRE a versão mais recente da mesma.
+    // Para memorizar: "if your state update depends on the previous state, use this function form".
+    // setUserInput(prevState => {
+    //   return {
+    //     ...prevState,
+    //     enteredTitle: event.target.value
+    //   };
+    // });
   };
 
   const amountChangeHandler = event => {
-    // setEnteredAmount(event.target.value);
+    setEnteredAmount(event.target.value);
 
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.title,
-    });
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.title,
+    // });
   };
 
   const dateChangeHandler = event => {
-    // setEnteredDate(event.target.value);
+    setEnteredDate(event.target.value);
 
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.title,
-    });
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.title,
+    // });
+  };
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    // Combinando os dados entrados.
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate) // o constructor "Date" irá parsear o valor da variável e converter para um objeto.
+    }
+
+    console.log(expenseData);
   };
 
   return (
-    <form>
+    // Ao clicar no botão, o evento de 'submit' será disparado pelo form.
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
@@ -63,7 +91,7 @@ const ExpenseForm = () => {
       </div>
 
       <div className="new-expense__actions">
-        <button type="submit" onClick={event => event.preventDefault()}>Add Expense</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   )
