8cc553f5f1e7ee0c39464bfbb66035d3d1053e83
Sidnei Farias <sidneijr.77@gmail.com>
2022-02-17 21:03:32 -0300

    updating to lesson 60

--- src/App.js
+++ src/App.js
@@ -29,9 +29,14 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  }
+
   return (
     <div className="App">
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expenses={expensesItems} />
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './expenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   // No entanto, o curso irá continuar com ênfase ao gerenciamento de múltiplos states.
   const [enteredTitle, setEnteredTitle] = useState(''); // é uma string vazia, pois o valor ainda não existe quando o componente é desenhado pela primeira vez.
   const [enteredAmount, setEnteredAmount] = useState('');
@@ -67,7 +67,11 @@ const ExpenseForm = () => {
       date: new Date(enteredDate) // o constructor "Date" irá parsear o valor da variável e converter para um objeto.
     }
 
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
+
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -76,17 +80,28 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          {/* Two way binding
+            Em inputs, não ouvimos somente à mudanças, mas também podemos passar um novo valor de volta ao input.
+            Dessa forma, podemos resetar ou alterar o input programaticamente.
+            É feito de forma simples: adicionando o atributo html "value" ao elemento desejado e então o definindo com o valor desejado.
+            Pode soar como um loop infinito, mas não é.
+
+            - A vantagem é que, quando o form realizar o evento de submit, podemos chamar "setEnteredTitle" e definir seu valor como uma string vazia (""), o qual era nosso estado inicial.
+            Dessa forma, estamos sobreescrevendo o que o usuário digitou e resetando o input ao seu valor original, para que novos valores possam ser usados após o fim do submit.
+
+            - Two way binding é muito útil ao trabalhar com forms, pois ele permite receber dados do usuário e também alterá-los.
+          */}
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" step="2022-12-31" onChange={dateChangeHandler} />
+          <input type="date" min="2019-01-01" step="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
         </div>
       </div>
 
