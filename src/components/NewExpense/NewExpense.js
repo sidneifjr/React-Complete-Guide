395310da30bc3ca38c45666fd2ca7659d613da4b
Sidnei Farias <sidneijr.77@gmail.com>
2022-03-17 00:23:51 -0300

    aplicando correção em NewExpense, relacionado à lição 59

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,14 +3,14 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './newExpense.css';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
 
-    console.log(expenseData);
+    props.onAddExpense(expenseData);
   };
 
   return (
